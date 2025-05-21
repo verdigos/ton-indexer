2b003a5850162480891824e0aef26496a55ae46a
Victor Tec <victor@ton.org>
2025-05-21 18:42:13 +0300

    Tonco failed target. Pton forward payload. Min amount

--- indexer/indexer/events/event_processing.py
+++ indexer/indexer/events/event_processing.py
@@ -32,7 +32,7 @@ from indexer.events.blocks.staking import TONStakersDepositMatcher, TONStakersWi
     TONStakersDelayedWithdrawalMatcher, NominatorPoolDepositMatcher, NominatorPoolWithdrawRequestMatcher, \
     NominatorPoolWithdrawMatcher
 from indexer.events.blocks.subscriptions import SubscriptionBlockMatcher, UnsubscribeBlockMatcher
-from indexer.events.blocks.swaps import DedustSwapBlockMatcher, StonfiSwapBlockMatcher, StonfiV2SwapBlockMatcher
+from indexer.events.blocks.swaps import DedustSwapBlockMatcher, StonfiSwapBlockMatcher, StonfiV2SwapBlockMatcher, ToncoDeployPoolBlockMatcher, ToncoSwapBlockMatcher
 from indexer.events.blocks.utils import NoMessageBodyException
 from indexer.events.blocks.utils import to_tree, EventNode
 from indexer.events.blocks.vesting import VestingSendMessageBlockMatcher, VestingAddWhiteListBlockMatcher
@@ -130,6 +130,7 @@ matchers = [
     DedustSwapBlockMatcher(),
     StonfiSwapBlockMatcher(),
     StonfiV2SwapBlockMatcher(),
+    ToncoSwapBlockMatcher(),
     NftTransferBlockMatcher(),
     TelegramNftPurchaseBlockMatcher(),
     NftDiscoveryBlockMatcher(),
@@ -149,6 +150,7 @@ matchers = [
     EvaaSupplyBlockMatcher(),
     EvaaWithdrawBlockMatcher(),
     EvaaLiquidateBlockMatcher(),
+    ToncoDeployPoolBlockMatcher(),
 ]
 
 trace_post_processors = [
