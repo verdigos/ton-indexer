26f99cfea3b8ea5659aad805bd103963501bf1bc
Victor Tec <victor@ton.org>
2025-05-25 03:29:12 +0300

    Refactor tonco liquidity parsers

--- indexer/indexer/events/blocks/liquidity.py
+++ indexer/indexer/events/blocks/liquidity.py
@@ -30,12 +30,12 @@ from indexer.events.blocks.messages.liquidity import (
     DedustDepositTONToVault, DedustDestroyLiquidityDepositContract,
     DedustReturnExcessFromVault, DedustTopUpLiquidityDepositContract,
     StonfiV2ProvideLiquidity)
-from indexer.events.blocks.messages.swaps import (DedustPayout,
-                                                  DedustPayoutFromPool,
-                                                  ToncoPoolV3FundAccount,
-                                                  ToncoAccountV3AddLiquidity, ToncoPoolV3FundAccountPayload, ToncoPoolV3MinAndRefund,
-                                                  ToncoRouterV3PayTo, ToncoPositionNftV3PositionInit, ToncoPoolV3Burn,
-                                                  ToncoPositionNftV3PositionBurn, ToncoPoolV3StartBurn)
+from indexer.events.blocks.messages import (DedustPayout,
+                                            DedustPayoutFromPool,
+                                            ToncoPoolV3FundAccount,
+                                            ToncoAccountV3AddLiquidity, ToncoPoolV3FundAccountPayload, ToncoPoolV3MinAndRefund,
+                                            ToncoRouterV3PayTo, ToncoPositionNftV3PositionInit, ToncoPoolV3Burn,
+                                            ToncoPositionNftV3PositionBurn, ToncoPoolV3StartBurn)
 from indexer.events.blocks.utils import AccountId, Amount, Asset
 from indexer.events.blocks.utils.block_utils import find_call_contract, find_call_contracts, get_labeled, \
     get_multiple_labeled
